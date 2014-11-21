6508aca24811a20d6cd8960af2a0cc87e827da38
Reinuas <mateussaunier@hotmail.com>
2014-11-21 11:37:20 +0000

    adding some modifications on SettingActivity

--- Curiosity/src/org/fslhome/curiosity/SettingsActivity.java
+++ Curiosity/src/org/fslhome/curiosity/SettingsActivity.java
@@ -1,5 +1,6 @@
 package org.fslhome.curiosity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
@@ -9,6 +10,18 @@ public class SettingsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+    
+    private void exemplo (){
+    
+    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder.setTitle("About Us");
+    	builder.setMessage("Developed By Thibatt Smith and Mateus Saunier");
+    	
+    	
+    	
+    	
+    	
     }
 
 }
